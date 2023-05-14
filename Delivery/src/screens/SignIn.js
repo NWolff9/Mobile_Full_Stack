83ffab7d49742e3019d876df84fd94723b7b07c4
NWolff9 <natha.wolff9@gmail.com>
2023-05-14 15:09:15 -0300

    tela de login

--- Delivery/src/screens/SignIn.js
+++ Delivery/src/screens/SignIn.js
@@ -1,25 +1,84 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import {
   View,
-  Text,
   StyleSheet,
-  Button,
+  TouchableOpacity,
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  Button,
+  Text,
+  TextInput,
+} from "react-native-paper";
 import { Context } from "../context/authContext";
 
-const SignIn = () => {
+const SignIn = ({ navigation }) => {
   const { state, teste } = useContext(Context);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [showPassword, setShowPassword] = useState(true);
+
   return (
-    <View>
-      <Text>SignIn</Text>
-      <Button
-        title="função"
-        onPress={() => teste("Hello from signin")}
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.login}>Login</Text>
+      <TextInput
+        style={styles.textInput}
+        label="E-mail"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        
       />
-    </View>
+      <TextInput
+        style={styles.textInput}
+        label="Senha"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry={showPassword}
+     
+      />
+      <Button mode="contained" style={styles.loginButton}>
+        Login
+      </Button>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("SignUp")}
+      >
+        <Text>
+          Não tem uma conta?{" "}
+          <Text style={styles.createAccountText}>
+            Crie uma
+          </Text>
+        </Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginTop: "40%",
+    alignSelf: "center",
+    width: "80%",
+  },
+  login: {
+    fontSize: 40,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  textInput: {
+    marginBottom: 10,
+  },
+  loginButton: {
+    padding: 5,
+    margin: 20,
+    width: "50%",
+    alignSelf: "center",
+  },
+  createAccountText: {
+    fontWeight: "bold",
+    color: "#6200ee",
+  },
+});
 
 export default SignIn;
